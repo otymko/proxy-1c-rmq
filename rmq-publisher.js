d604ec40d6ddecf8cd817f96145bdec0669ad877
Tymko Oleg <olegtymko@yandex.ru>
2018-08-12 23:32:21 +0700

    Первый коммит

---
+++ rmq-publisher.js
@@ -0,0 +1,82 @@
+var configNode = require('./config.json');
+
+var amqp = require('amqplib/callback_api');
+var amqpConn = null;
+var pubChannel = null;
+var offlinePubQueue = [];
+
+exports.startPub = start; 
+exports.publish = publish;
+
+var setting = {protocol: 'amqp', 
+  hostname: configNode.rabbitmq.hostname, 
+  port: configNode.rabbitmq.port, 
+  username: configNode.rabbitmq.username, 
+  password: configNode.rabbitmq.password, 
+  vhost: configNode.rabbitmq.vhost
+};
+
+function start() {
+    amqp.connect(setting, function(err, conn) {
+    if (err) {
+      console.error("[AMQP]", err.message);
+      return setTimeout(start, 1);
+    }
+    conn.on("error", function(err) {
+      if (err.message !== "Connection closing") {
+        console.error("[AMQP] conn error", err.message);
+      }
+    });
+    conn.on("close", function() {
+      console.error("[AMQP] reconnecting");
+      return setTimeout(start, 1);
+    });
+
+    console.log("[AMQP] connected");
+    amqpConn = conn;
+
+    startPublisher();
+  });
+}
+
+function startPublisher() {
+  amqpConn.createConfirmChannel(function(err, ch) {
+    if (closeOnErr(err)) return;
+    ch.on("error", function(err) {
+      console.error("[AMQP] channel error", err.message);
+    });
+    ch.on("close", function() {
+      console.log("[AMQP] channel closed");
+    });
+
+    pubChannel = ch;
+    while (true) {
+      var m = offlinePubQueue.shift();
+      if (!m) break;
+      publish(m[0], m[1], m[2]);
+    }
+  });
+}
+
+function publish(exchange, routingKey, content) {
+  try {
+    pubChannel.publish(exchange, routingKey, content, { persistent: true },
+                       function(err, ok) {
+                         if (err) {
+                           console.error("[AMQP] publish", err);
+                           offlinePubQueue.push([exchange, routingKey, content]);
+                           pubChannel.connection.close();
+                         }
+                       });
+  } catch (e) {
+    console.error("[AMQP] publish", e.message);
+    offlinePubQueue.push([exchange, routingKey, content]);
+  }
+}
+
+function closeOnErr(err) {
+  if (!err) return false;
+  console.error("[AMQP] error", err);
+  amqpConn.close();
+  return true;
+}
---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+    bodyParser = require('body-parser'); 
+    app = express();
+    port = process.env.PORT || 3000;
+
+var publisherClient = require('./rmq-publisher');
+
+app.use(bodyParser.text({ type: 'text/xml' , limit: '50mb'}))
+
+//http://localhost:3000/publish?guid=32323-32323-4332-222&exchange=directory&routing_key=site
+app.post('/publish', function(req, res) {
+    var guid = req.param('guid');
+    var exchange = req.param('exchange');
+    var routing_key = req.param('routing_key');
+    
+    publisherClient.publish("", routing_key, new Buffer(req.body));
+    console.log("routing_key " + routing_key);
+    console.log("Sent " + guid);
+    res.send(guid);
+});
+    
+app.listen(port);
+publisherClient.startPub();
+    
+console.log('Server started! At http://localhost:' + port);
